e764101a71196c2f3be395e6513040f9e116ac0f
Yaroslav Shmarov <yshmarov@gmail.com>
2024-08-25 21:00:34 +0200

    link to mentioned user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -73,7 +73,8 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      # @user = User.find(params[:id])
+      @user = User.find_by(id: params[:id]) || User.find_by(username: params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,8 @@
 module MessagesHelper
+  def with_mentions(text)
+    text.gsub(/@(\w+)/) do |mention|
+      username = mention[1..]
+      link_to mention, user_path(username), class: 'text-blue-500'
+    end
+  end
 end
