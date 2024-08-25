c62d41cc6ebcf01bc31c28b3dd5a69d615020687
Yaroslav Shmarov <yshmarov@gmail.com>
2024-08-25 20:40:20 +0200

    generate mentions

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,15 @@
 class Message < ApplicationRecord
+  has_many :mentions, dependent: :destroy
+
+  after_create_commit :generate_mentions
+
+  private
+
+  def generate_mentions
+    mentioned_usernames = body.scan(/@(\w+)/).flatten
+    users = User.where(username: mentioned_usernames)
+    users.each do |user|
+      self.mentions.create(user: user)
+    end
+  end
 end
